df9ead48d1b0cf84942f849c7635b4926706b135
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2020-11-26 18:22:59 -0500

    to cross loss

--- utils/dataset.py
+++ utils/dataset.py
@@ -172,12 +172,12 @@ class Emotic_MultiDB(Dataset):
 					if cct in curr_categories:
 						lbl[i] = 1.0
 						if self.TakeOne:
-							return np.argmax(lbl)[0]
+							return np.argmax(lbl)
 				continue
 			if ct in curr_categories:
 				lbl[i] = 1.0
 				if self.TakeOne:
-					return np.argmax(lbl)[0]
+					return np.argmax(lbl)
 		
 		return lbl
 
