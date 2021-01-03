c38e3874ff81bf3b1dd702efbfaee5f4b4f7e982
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2021-01-03 10:45:31 -0500

    not yet

--- processor.py
+++ processor.py
@@ -4,8 +4,8 @@ import os
 import time
 import numpy as np
 import yaml
-import pickle
-from collections import OrderedDict, defaultdict
+#import pickle
+#from collections import OrderedDict, defaultdict
 
 import torch
 import torch.nn as nn
